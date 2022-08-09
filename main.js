7666280fc70dcd3debbeace3b000c200b29b4ae4
diegotur <gpslinea15@hotmail.com>
2022-08-09 13:45:33 -0300

    yoquese

--- main.js
+++ main.js
@@ -1,13 +1,88 @@
-/* let numero=[];
+let numero=[0,1,2,3,4,5,6,7,8,9];
 
-for (i=0;i<4;i++){
-    let x = Math.floor ((Math.random() * 10))
-        do{
-        if (numero.some((n) => n == x)==false){
-            numero.push (x);
-        }
-    } while (numero.length < 4);
-    };
+let gN = numero.sort(func);  
+
+function func(a, b) {  
+  return 0.5 - Math.random();
+} 
+   
+let arrGuessNumber = gN.slice(3,7);
+
+guessNumber = arrGuessNumber.join("");
+
+//console.log(guessNumber);
+
+boton = document.getElementById ("btn0");
+
+let verIntento = document.getElementById ("verIntento");
+
+let intentos = [];
 
 
-console.log(numero); */
+
+boton.addEventListener("click", puto);
+
+function puto (){
+    
+    let verga = document.getElementById (putin);
+    let vergaza = verga.innerHTML;
+    console.log(vergaza);
+  
+}
+/* function NumeroClick (){
+    let x = boton[]
+
+    intentos.push(document.getElementsByTagName(boton5)); */
+
+        
+
+
+
+/* }  */
+/* boton1.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(1);
+}
+boton2.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(2);
+}
+boton3.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(3);
+}
+boton4.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(4);
+}
+boton5.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(5);
+}
+boton6.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(6);
+}
+boton7.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(7);
+}
+boton8.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(8);
+}
+boton9.addEventListener("click", NumeroClick);
+
+function NumeroClick (){
+    intentos.push(9);
+}
+ */
+
