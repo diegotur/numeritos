f0ce8183226cab31a0cf31084f947690f53f8ed8
diego.tur <gpslinea15@hotmail.com>
2022-08-09 00:29:40 -0300

    number

---
+++ main.js
@@ -0,0 +1,13 @@
+/* let numero=[];
+
+for (i=0;i<4;i++){
+    let x = Math.floor ((Math.random() * 10))
+        do{
+        if (numero.some((n) => n == x)==false){
+            numero.push (x);
+        }
+    } while (numero.length < 4);
+    };
+
+
+console.log(numero); */
