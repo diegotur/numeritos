18057e0b7ce1f8cc48d54ee87f519ff758a82bfc
diegotur <gpslinea15@hotmail.com>
2022-08-13 14:48:19 -0300

    numeritos

--- main.js
+++ main.js
@@ -18,28 +18,67 @@ let verIntento = document.getElementById ("verIntento");
 
 let intentos = [];
 
+let arrBtn = [];
 
-document.getElementById("btn0").addEventListener("click", ()=> {myFunction("0")});
-document.getElementById("btn1").addEventListener("click", ()=> {myFunction("1")});
-document.getElementById("btn2").addEventListener("click", ()=> {myFunction("2")});
-document.getElementById("btn3").addEventListener("click", ()=> {myFunction("3")});
-document.getElementById("btn4").addEventListener("click", ()=> {myFunction("4")});
-document.getElementById("btn5").addEventListener("click", ()=> {myFunction("5")});
-document.getElementById("btn6").addEventListener("click", ()=> {myFunction("6")});
-document.getElementById("btn7").addEventListener("click", ()=> {myFunction("7")});
-document.getElementById("btn8").addEventListener("click", ()=> {myFunction("8")});
-document.getElementById("btn9").addEventListener("click", ()=> {myFunction("9")});
+
+
+for (i=0;i<10;i++){
+  arrBtn.push ("btn"+i); 
+}
+
+for (const elem of arrBtn){
+  document.getElementById(elem).addEventListener("click", ()=> {myFunction(arrBtn.indexOf(elem))}); 
+}
 
 
 function myFunction(a) {
     if (intentos.length <4){
-        const node = document.createElement("div");
-        const textnode = document.createTextNode(a);
-        node.classList.add('styleDiv');
-        node.appendChild(textnode);
-        console.log (node, textnode);
-        verIntento.appendChild(node);
-        intentos.push(a);
-        console.log(intentos);
+      if (intentos.some ((el) => el == a) == false){
+        let x = document.getElementsByClassName("styleDiv");
+        if (x < 4 == true){
+          const node = document.createElement("div");
+          const textnode = document.createTextNode(a);
+          node.classList.add('styleDiv');
+          node.appendChild(textnode);
+          verIntento.appendChild(node);
+          intentos.push(a);
+          console.log (intentos);
+        } else{
+          if (x[0] == ""){
+            const textnode = document.createTextNode(a);
+            x[0].innerText = node;
+            intentos.push(a);
+          } else if (x[1] == ""){
+            const textnode = document.createTextNode(a);
+            x[1].innerText = node;
+            intentos.push(a);
+          } else if (x[2] == ""){
+            const textnode = document.createTextNode(a);
+            x[2].innerText = node;
+            intentos.push(a);
+          } else if (x[3] == ""){
+            const textnode = document.createTextNode(a);
+            x[3].innerText = node;
+            intentos.push(a);
+          }
+
+          }
+        }
+      }  
     }
+   
+
+    document.getElementById("btnBorrar").addEventListener("click", Borrar);
+
+
+    function Borrar(){
+      intentos.pop();
+      let x = document.getElementsByClassName("styleDiv");
+      for (i=0; i<x.length; i++){
+        x[i].innerText = "";
+      }
+      for (i=0; i<intentos.length; i++){
+        x[i].innerText = intentos[i];
+      }
+
     }
